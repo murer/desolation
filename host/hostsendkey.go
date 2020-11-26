df40f8c64e7d8a4ee393e489304bc568aa9fd648
murer <murer@dextra-sw.com>
2020-11-26 18:47:22 -0200

    .

--- host/hostsendkey.go
+++ host/hostsendkey.go
@@ -17,7 +17,7 @@ import (
 func HostSendKeys(keys []string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 1000*time.Millisecond)
 	defer cancel()
-	args := append([]string{"key"}, keys...)
+	args := append([]string{"key", "--delay", "5"}, keys...)
 	log.Printf("aaaa %#v", args)
 	cmd := exec.CommandContext(ctx, "xdotool", args...)
 	cmd.Start()
@@ -27,8 +27,17 @@ func HostSendKeys(keys []string) {
 
 func HostSendMsg(msg *message.Message) {
 	encoded := message.Encode(msg)
+	encoded = util.B64Enc([]byte(encoded))
+	log.Print(encoded)
 	array := strings.Split(encoded, "")
-	array = append(array, "Return")
+	for i := 0; i < len(array); i++ {
+		if array[i] == "" {
+			array[i] = "minus"
+		} else if array[i] == "_" {
+			array[i] = "underscore"
+		}
+	}
+	array = append("ctrl+a", "BackSpace", array, "Return")
 	HostSendKeys(array)
 
 }
--- host/hostsendkey_test.go
+++ host/hostsendkey_test.go
@@ -2,8 +2,19 @@ package host_test
 
 import (
 	"testing"
+
+	"github.com/murer/desolation/host"
+	"github.com/murer/desolation/message"
 )
 
 func TestSendkey(t *testing.T) {
 	//host.HostSendKeys([]string{"{"})
+}
+
+func TestSendMsg(t *testing.T) {
+	host.HostSendMsg(&message.Message{
+		Name:    "echo",
+		Headers: map[string]string{"rid": "x"},
+		Payload: "y",
+	})
 }
